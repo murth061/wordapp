1d17f262c20a0f2017e1f9f3ce1577212fb5ed5d
Suriya Murthy <murth061@umn.edu>
2019-11-15 21:26:20 -0600

    asdf

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ class WordForm(FlaskForm):
         Regexp(r'^[a-z]+$', message="must contain letters only"),
     ])
     #word length
-    word_length = SelectField("Length", choices=[(0, 'No thanks'), (3, '3'),(4, '4'), (5, '5'), (6, '6'), (7, '7'), (8, '8'), (9, '9'), (10, '10')], validators=None, coerce=int)
+    word_length = SelectField("Length (optional)", choices=[(0, 'No thanks'), (3, '3'),(4, '4'), (5, '5'), (6, '6'), (7, '7'), (8, '8'), (9, '9'), (10, '10')], validators=None, coerce=int)
     #pattern
     pattern_word = StringField("Pattern choice (optional)", validators=[Regexp(r'^[a-z.]+$', message="must contain letters or dots '.' only")
     ])
@@ -62,13 +62,27 @@ def letters_2_words():
     for l in range(3,len(letters)+1):
         for word in itertools.permutations(letters,l):
             w = "".join(word)
-            #match condition
+            #match condition (works)
             if w in good_words:
-                #length condition
+                #length condition (works)
                 if length != 0:
                     if (len(w) == length):
                         #pattern condition (pending)
-                        word_set.add(w)
+                        #go through each letter and see if it matches with the other word
+                        if(patternLength !=0):
+                            i = 0
+                            state = True
+                            while(i<=patternLength):
+                                if(word[i] != w[i])
+                                    state = False
+                                if(state = False)
+                                    break
+                                if(i ==patternLength):
+                                    word_set.add(w)
+                        else:
+                            word_set.add(w)
+
+                                
 
     return render_template('wordlist.html',
         wordlist=sorted(word_set),
